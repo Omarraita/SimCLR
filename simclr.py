5e4c568332c129897d501a62f6c4cc8fa7eea12e
Omarraita <71269935+Omarraita@users.noreply.github.com>
2021-03-16 12:37:51 +0100

    Update simclr.py

--- simclr.py
+++ simclr.py
@@ -78,7 +78,7 @@ class SimCLR(object):
 
                 with autocast(enabled=self.args.fp16_precision):
                     features = self.model(images)
-                    print(tpye(features))
+                    #print(type(features))
                     #print(features)
                     logits, labels = self.info_nce_loss(features)
                     loss = self.criterion(logits, labels)
